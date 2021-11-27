193fdf1393611c2a01ba612a49a78239b231c1f0
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-28 01:03:50 +0300

    Design added

--- app/src/main/java/com/asimodabas/catch_bomb/MainActivity.kt
+++ app/src/main/java/com/asimodabas/catch_bomb/MainActivity.kt
@@ -2,10 +2,17 @@ package com.asimodabas.catch_bomb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.asimodabas.catch_bomb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
+
     }
 }
