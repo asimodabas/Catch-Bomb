4d4534089f8e169816ea7e161f881a843ffbae2e
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-11-28 23:51:55 +0300

    counter incrementer and view binding added

--- app/src/main/java/com/asimodabas/catch_bomb/MainActivity.kt
+++ app/src/main/java/com/asimodabas/catch_bomb/MainActivity.kt
@@ -2,9 +2,12 @@ package com.asimodabas.catch_bomb
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.asimodabas.catch_bomb.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var score = 0
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,7 +15,52 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
-        setContentView(R.layout.activity_main)
+        setContentView(view)
+
+
+        binding.imageView.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView10.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView11.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView12.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView2.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView3.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView4.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView5.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView6.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView7.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView8.setOnClickListener {
+            increaseScore()
+        }
+        binding.imageView9.setOnClickListener {
+            increaseScore()
+        }
+
 
     }
+
+    private fun increaseScore() {
+        score += 1
+        binding.scoreText.text="Score : $score"
+    }
+
 }
